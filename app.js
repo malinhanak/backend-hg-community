8a033b7a285fd6fb8cdc12ff13d54e5d574ab5f5
Malin Hanak <malinehanak@gmail.com>
2020-05-08 09:18:30 +0200

    implementing error middleware

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const HttpError = require('./models/http-error');
-const User = require('./models/user');
 const horsesRouter = require('./routes/horseRouter');
-const userRouter = require('./routes/userRouter')(User);
+const userRouter = require('./routes/userRouter');
 const db = require('./db');
 
 const app = express();
@@ -19,6 +18,12 @@ app.use('/api/horses', horsesRouter);
 
 app.use((req, res, next) => {
   throw new HttpError('Could not find the requested route', 404);
+});
+
+app.use((error, req, res, next) => {
+  return res
+    .status(error.code)
+    .json({ message: error.message, error: error.error });
 });
 
 db.connect()
