aa7435b31c9886a53d901902b8459f378a01ed1b
Malin Hanak <malinehanak@gmail.com>
2020-05-08 09:23:34 +0200

    Adding additional informtion for the HttpError class

--- models/http-error.js
+++ models/http-error.js
@@ -1,7 +1,8 @@
 class HttpError extends Error {
-  constructor(message, errorCode) {
+  constructor(message, errorCode, errInfo) {
     super(message);
     this.code = errorCode;
+    this.error = errInfo;
   }
 }
 
